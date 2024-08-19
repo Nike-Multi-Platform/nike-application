4c61fae783a6988474c0f1dd1df738a25e578b4f
Bjkdy <pv198357@gmail.com>
2024-08-19 22:04:55 +0700

    Create ProductParentRepository.cs

---
+++ DAL/ProductParentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Nike_Shop_Management.MappingLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nike_Shop_Management.DAL
+{
+    class ProductParentRepository
+    {
+        private readonly DbContext _db;
+        ObjectMapper objectMapper = new ObjectMapper();
+
+    }
+}

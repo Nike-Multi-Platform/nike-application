43ce97fddfbce5080dbd4451956d19772f7e00bc
Bjkdy <pv198357@gmail.com>
2024-08-19 14:40:19 +0700

    templates for brotherhood about edit address user

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -29,48 +29,101 @@ namespace Nike_Shop_Management.GUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
             this.dataGridView = new ComponentFactory.Krypton.Toolkit.KryptonDataGridView();
+            this.btnSave = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.address = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txID = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // kryptonButton1
-            // 
-            this.kryptonButton1.Location = new System.Drawing.Point(945, 147);
-            this.kryptonButton1.Name = "kryptonButton1";
-            this.kryptonButton1.Size = new System.Drawing.Size(90, 25);
-            this.kryptonButton1.TabIndex = 0;
-            this.kryptonButton1.Values.Text = "kryptonButton1";
-            this.kryptonButton1.Click += new System.EventHandler(this.kryptonButton1_Click);
-            // 
             // dataGridView
             // 
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView.Location = new System.Drawing.Point(27, 314);
+            this.dataGridView.MultiSelect = false;
             this.dataGridView.Name = "dataGridView";
+            this.dataGridView.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2007Silver;
+            this.dataGridView.ReadOnly = true;
             this.dataGridView.RowHeadersWidth = 51;
             this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.RowTemplate.ReadOnly = true;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView.Size = new System.Drawing.Size(1081, 214);
+            this.dataGridView.StateCommon.BackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.GridBackgroundList;
+            this.dataGridView.StateCommon.HeaderRow.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.dataGridView.TabIndex = 1;
+            this.dataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.ButtonStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.InputControl;
+            this.btnSave.Location = new System.Drawing.Point(1018, 265);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2007Blue;
+            this.btnSave.Size = new System.Drawing.Size(90, 25);
+            this.btnSave.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSave.StateCommon.Content.Draw = ComponentFactory.Krypton.Toolkit.InheritBool.True;
+            this.btnSave.StateCommon.Content.DrawFocus = ComponentFactory.Krypton.Toolkit.InheritBool.True;
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Values.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(27, 135);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(100, 27);
+            this.address.TabIndex = 3;
+            this.address.Text = "kryptonTextBox1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(414, 485);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(11, 8);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // txID
+            // 
+            this.txID.Location = new System.Drawing.Point(27, 27);
+            this.txID.Name = "txID";
+            this.txID.ReadOnly = true;
+            this.txID.Size = new System.Drawing.Size(100, 27);
+            this.txID.TabIndex = 5;
+            this.txID.Text = "kryptonTextBox1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1134, 560);
+            this.Controls.Add(this.txID);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.dataGridView);
-            this.Controls.Add(this.kryptonButton1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton1;
         private ComponentFactory.Krypton.Toolkit.KryptonDataGridView dataGridView;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnSave;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox address;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox txID;
     }
 }
--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -26,16 +26,18 @@ namespace Nike_Shop_Management.GUI
         }
         public void load_data()
         {
+          
             List<UserAccountDTO> list = new List<UserAccountDTO>();
+       
             list = accountManager.GetUserAccounts();
             dataGridView.DataSource = list;
-            
+            dataGridView.Columns[11].Visible = false; // Hidden ID
         }
         private void Form1_Load(object sender, EventArgs e)
         {
            
         }
-
+        
         private void kryptonButton1_Click(object sender, EventArgs e)
         {
             UserAccountDTO user = accountManager.GetAccount("111@gmail.com", "123");
@@ -46,6 +48,31 @@ namespace Nike_Shop_Management.GUI
             else
             {
                 MessageBox.Show("Khong tim thay du lieu ");
+            }
+        }
+
+        private void dataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                txID.Text = dataGridView.Rows[e.RowIndex].Cells[11].Value.ToString();
+                address.Text = dataGridView.Rows[e.RowIndex].Cells[5].Value != null ? dataGridView.Rows[e.RowIndex].Cells[5].Value.ToString() : " ";
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(txID.Text!=String.Empty)
+            {
+                UserAccountDTO ucc = accountManager.GetUserByID(Int32.Parse(txID.Text.ToString()));
+                ucc.Address = address.Text;
+                // các thuộc tính khác
+                int fl = accountManager.EditUser(ucc);
+                if(fl==1)
+                {
+                    MessageBox.Show("Cap nhat thanh cong");
+                    load_data();
+                }
             }
         }
     }

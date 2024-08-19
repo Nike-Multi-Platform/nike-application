fdf8776e6a85ec2e92b1c4d486550fcb09c4d479
Bjkdy <pv198357@gmail.com>
2024-08-19 21:59:09 +0700

    updated AutoMapper library

---
+++ DTO/ProductImgDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nike_Shop_Management.DTO
+{
+    class ProductImgDTO
+    {
+        int id, product_id;
+        string img;
+
+        public int Id { get => id; set => id = value; }
+        public int Product_id { get => product_id; set => product_id = value; }
+        public string Img { get => img; set => img = value; }
+    }
+}

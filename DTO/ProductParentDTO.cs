f98cc5d29159e0f115d9128042eb6df72e8ff4cf
Bjkdy <pv198357@gmail.com>
2024-08-20 14:29:19 +0700

    updated class

--- DTO/ProductParentDTO.cs
+++ DTO/ProductParentDTO.cs
@@ -8,14 +8,17 @@ namespace Nike_Shop_Management.DTO
 {
     class ProductParentDTO
     {
-        private int id;
-        private string category_product_name;
-        private string category_product_desciption;
-        
+        public int product_parent_id { get; set; }
+        public string product_parent_name { get; set; }
+        public int product_object_id { get; set; }
+        public int product_category_id { get; set; }
+        public string thumbnail { get; set; }
+        public string product_price { get; set; }
+        public Boolean is_new_release { get; set; }
+        public int product_icons_id { get; set; }
 
-        public int Id { get => id; set => id = value; }
-        public string Category_product_name { get => category_product_name; set => category_product_name = value; }
-        public string Category_product_desciption { get => category_product_desciption; set => category_product_desciption = value; }
-         
+        public ProductIconsDTO productIcons { get; set; }
+        public ProductDTO productChild { get; set; }
+
     }
 }

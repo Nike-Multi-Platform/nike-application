797797e05b3a9a080282a1622c43945538d603eb
bqdiu <boquangdieu2003@gmail.com>
2024-09-28 23:06:13 +0700

    add custom control

---
+++ CustomControl/CancelButton.cs
@@ -0,0 +1,30 @@
+﻿using CloudinaryDotNet.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nike_Shop_Management.CustomControl
+{
+    public class CancelButton : Button
+    {
+        public CancelButton()
+        {
+            this.Click += CancelButton_Click;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            if (this.Parent != null)
+            {
+                this.Parent.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("No parent container found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ CustomControl/PriceTextBox.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nike_Shop_Management.CustomControl
+{
+    public class PriceTextBox : TextBox
+    {
+        private ErrorProvider error = new ErrorProvider();
+        public PriceTextBox()
+        {
+            this.KeyPress += new KeyPressEventHandler(PriceTextBox_KeyPress);
+        }
+
+        private void PriceTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; 
+                error.SetError(this, "Chỉ được phép nhập số.");
+            }
+            else
+            {
+                error.Clear();
+            }
+        }
+
+    }
+}

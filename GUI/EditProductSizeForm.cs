46b2443ebd2fb90df9e6b5242a1c1e2aecac368a
Bjkdy <pv198357@gmail.com>
2024-10-02 13:06:37 +0700

    hehe?

--- GUI/EditProductSizeForm.cs
+++ GUI/EditProductSizeForm.cs
@@ -1,30 +1,79 @@
 ﻿using Nike_Shop_Management.BLL;
+using Nike_Shop_Management.DAL;
 using Nike_Shop_Management.DTO;
+using Nike_Shop_Management.MappingLayer;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace Nike_Shop_Management.GUI
 {
     public partial class EditProductSizeForm : Form
     {
-        List<SizeDTO> list;
+        List<SizeDTO> listInventory;
+        List<GetTheSizeProductCurrentResult> listCurrent;
         public EditProductSizeForm()
         {
             InitializeComponent();
-            
+            btnLeftToRight.Click += BtnLeftToRight_Click;
+            btnRightToLeft.Click += BtnRightToLeft_Click;
+            btnSave.Click += BtnSave_Click;
         }
-        public void InitData(int product_id,TypeSize typeSize)
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+
+            }
+        }
+
+        private void BtnRightToLeft_Click(object sender, EventArgs e)
+        {
+            if (ListSizeCurrent.SelectedRows != null)
+            {
+                var valueSelected = ListSizeCurrent.CurrentRow;
+                ListSizeCurrent.Rows.Remove(valueSelected);
+                ListSizeInventory.Rows.Add(new object[] { valueSelected.Cells[0].Value.ToString(), valueSelected.Cells[1].Value.ToString(), valueSelected.Cells[2].Value != null ? valueSelected.Cells[2].Value.ToString() : null });
+            }
+        }
+
+        private void BtnLeftToRight_Click(object sender, EventArgs e)
+        {
+            if (ListSizeInventory.SelectedRows != null)
+            {
+                var valueSelected = ListSizeInventory.CurrentRow;
+                ListSizeInventory.Rows.Remove(valueSelected);
+                ListSizeCurrent.Rows.Add(new object[] {valueSelected.Cells[0].Value.ToString(), valueSelected.Cells[1].Value.ToString(), valueSelected.Cells[2].Value != null ? valueSelected.Cells[2].Value.ToString() : null});
+            }
+        }
+
+        public void InitData(int product_id, TypeSize typeSize)
         {
             ProductSizeManager pzM = new ProductSizeManager();
-            list =  pzM.GetProductSizeInventory(product_id, typeSize);
-            ListSizeInventory.DataSource = list;
+            listInventory = pzM.GetProductSizeInventory(product_id, typeSize);
+            ListSizeInventory.Columns.Clear();
+            ListSizeInventory.Columns.Add("size_id", "Size ID");
+            ListSizeInventory.Columns.Add("size_name", "Size Name");
+            ListSizeInventory.Columns.Add("product_size_id", "Product_size_id");
+
+            ListSizeCurrent.Columns.Clear();
+            ListSizeCurrent.Columns.Add("size_id", "Size ID");
+            ListSizeCurrent.Columns.Add("size_name", "Size Name");
+            ListSizeCurrent.Columns.Add("product_size_id", "Product_size_id");
+            //  ListSizeCurrent.Columns[2].Visible = false;
+            foreach (var item in listInventory)
+            {
+                ListSizeInventory.Rows.Add(new object[] { item.size_id, item.size_name, null });
+            }
+
+            ProductColorManager pcM = new ProductColorManager();
+            listCurrent = pcM.GetProductSizesByID(product_id);
+            foreach (var item in listCurrent)
+            {
+                ListSizeCurrent.Rows.Add(new object[] { item.size_id, item.size_name, item.product_size_id });
+            }
+
+
         }
         private void EditProductSizeForm_Load(object sender, EventArgs e)
         {
